70753ab003446d194390b83d650f58895a5593bc
Willythepo0h <johnwillardsucgang@gmail.com>
2025-04-15 12:41:02 +0800

    Modified to accommodate export to csv function.

--- app/employee-tabs/index.tsx
+++ app/employee-tabs/index.tsx
@@ -1,23 +1,38 @@
-import React from "react";
-import { useRef } from "react";
 import { useRouter } from "expo-router";
+import React, { useRef, useState, useEffect} from "react";
 import { Animated, ScrollView, StyleSheet } from "react-native";
 
 import HelloWave from "@/components/HelloWave";
 import { ThemedView } from "@/components/ThemedView";
 import { ThemedText } from "@/components/ThemedText";
 import UploadButton from "@/components/UploadButton";
+import { getLocalApiUrl } from "@/functions/getLocalIP";
+import ExportCSVButton from "@/components/ExportButton";
 import TableComponent from "@/components/TableComponent";
 import DropdownButton from "@/components/DropdownButton";
 import { useThemeColor } from "@/functions/useThemeColor";
 import { useImageUpload } from "@/functions/useImageUpload";
 import ParallaxHeader, { HEADER_MAX_HEIGHT } from "@/components/ParallaxScrollView";
 
+type CocolisapEntry = {
+  timestamp: string;
+  value: number;
+  num_detections: number;
+  classification: string;
+  latitude: number;
+  longitude: number;
+  city: string;
+  region: string;
+  country: string;
+};
+
 export default function EmployeeScreen() {
   const router = useRouter();
-  const borderColor = useThemeColor({ light: "#ccc" }, "tint");
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState<CocolisapEntry[]>([]);
   const { pickedImage, uploadImage } = useImageUpload();
   const scrollY = useRef(new Animated.Value(0)).current;
+  const borderColor = useThemeColor({ light: "#ccc" }, "tint");
 
   const handleUpload = async () => {
     const image = await uploadImage();
@@ -31,6 +46,26 @@ export default function EmployeeScreen() {
       return;
     }
   };
+
+  useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const API_URL = await getLocalApiUrl(8000, "FAST_API_URL");
+          const res = await fetch(`${API_URL}/dashboard/summary`);
+          if (!res.ok) throw new Error("Network error");
+  
+          const json = await res.json();
+          const history = json?.cocolisap_history || [];
+          setData(history);
+        } catch (err) {
+          console.error(err);
+        } finally {
+          setLoading(false);
+        }
+      };
+  
+      fetchData();
+    }, []);
 
   return (
     <ThemedView style={styles.container}>
@@ -82,8 +117,10 @@ export default function EmployeeScreen() {
 
         <DropdownButton />
 
+        <ExportCSVButton data={data} />
+
         <ThemedView style={[styles.tableWrapper, { borderColor }]}>
-          <TableComponent />
+          <TableComponent data={data}/>
         </ThemedView>
 
 
